d23f445d959255f021fec45f7224f6d156612623
James Morgan <james.morgan.e@googlemail.com>
2019-09-13 15:52:52 +0100

    image

--- functions/api/image/index.js
+++ functions/api/image/index.js
@@ -8,19 +8,28 @@ images.get('/decorate/kitty/:kittyId/bandana', async (req, res, next) => {
     try {
         const {kittyId} = req.params;
 
-        const svgRaw = `https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/${kittyId}.svg`;
+        const png = await generatePng(kittyId);
 
-        const builtSvg = await imageDecoratorService.overlayBandana(svgRaw);
-
-        res.setHeader('Content-Type', 'image/svg+xml');
+        res.setHeader('Content-Type', 'image/png');
 
         return res
             .status(200)
-            .send(builtSvg);
+            .send(png);
 
     } catch (e) {
         next(e);
     }
 });
+
+const generatePng = async (kittyId) => {
+    const svgRaw = `https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/${kittyId}.svg`;
+
+    const builtSvg = await imageDecoratorService.overlayBandana(svgRaw);
+
+    return await convert(builtSvg, {
+        height: 1200,
+        puppeteer: {args: ['--no-sandbox', '--disable-setuid-sandbox']}
+    });
+};
 
 module.exports = images;
